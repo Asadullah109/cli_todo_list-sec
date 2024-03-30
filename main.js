f082fb2bb053baa1a8e27760d62e8ca1be05b4df
Asadiftekhar <143116178+asad755@users.noreply.github.com>
2024-03-31 04:33:24 +0500

    coding todo

---
+++ main.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = true;
+console.log(chalk.red.bold("\n \t Welcome to code withasadullah Todo List App\n"));
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.green("Enter your new task: ")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-list successfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addMore",
+            type: "confirm",
+            default: "false"
+        }
+    ]);
+    condition = addMoreTask.addMore;
+}
+console.log("your updated Todo-List:", todoList);
